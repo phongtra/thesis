61fca6d648b551af8fe1fff905e3e2d7119c18c0
Phong Tran <aphtr001@edu.xamk.fi>
2021-03-11 20:26:13 +0200

    scaffold state voteform

--- client/src/components/VoteForm.tsx
+++ client/src/components/VoteForm.tsx
@@ -3,22 +3,40 @@ import { FormControl, FormLabel } from '@chakra-ui/form-control';
 import { Input } from '@chakra-ui/input';
 import { Heading } from '@chakra-ui/layout';
 import { Select } from '@chakra-ui/select';
-import React from 'react';
+import React, { SyntheticEvent, useState } from 'react';
 
 const VoteForm: React.FC = () => {
+  const [socialNumber, setSocialNumber] = useState('');
+  const [candidate, setCandidate] = useState('');
+  const onSubmit = (e: SyntheticEvent) => {
+    e.preventDefault();
+    if (!candidate) {
+      console.log('Must select a candidate');
+    }
+    console.log(socialNumber);
+    console.log(candidate);
+  };
   return (
     <>
       <Heading as='h3' size='lg'>
         Here is the US Election vote, choose your candidate
       </Heading>
-      <form>
+      <form onSubmit={onSubmit}>
         <FormControl mt={4}>
           <FormLabel>Your Social Number</FormLabel>
-          <Input />
+          <Input
+            value={socialNumber}
+            onChange={(e) => setSocialNumber(e.target.value)}
+          />
           <FormLabel mt={4}>Choose one</FormLabel>
-          <Select>
-            <option>Joe Biden</option>
-            <option>Donald Trumpt</option>
+          <Select
+            defaultValue=''
+            value={candidate}
+            onChange={(e) => setCandidate(e.target.value)}
+          >
+            <option value=''></option>
+            <option value='Joe Biden'>Joe Biden</option>
+            <option value='Donald Trump'>Donald Trumpt</option>
           </Select>
           <Button mt={4} mb={4}>
             Vote
